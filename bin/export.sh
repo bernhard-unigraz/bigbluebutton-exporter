3af105ecad22da97cc04418101f8923f29b5cbf6
Bitterer Bernhard <bernhard.bitterer@uni-graz.at>
2021-09-28 07:57:53 +0200

    Added json_pp to create a pretty json file

--- bin/export.sh
+++ bin/export.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Make sure file exists and has API_TOKEN=<YOUR API TOKEN> inside
-source credentials.txt
-
-
-
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 UUID=$1
 DASHBOARD=$2
-
+# Make sure file exists and has API_TOKEN=<YOUR API TOKEN> inside
+source $SCRIPTPATH/credentials.txt
 if [ -z "$2" ]
   then
     DASHBOARD=$1
 fi
 
-curl -H "Authorization: Bearer $API_TOKEN" http://$HOST/api/dashboards/uid/$UUID > $SCRIPTPATH/../extras/dashboards/$DASHBOARD.json
+curl -H "Authorization: Bearer $API_TOKEN" http://$HOST/api/dashboards/uid/$UUID | json_pp -json_opt pretty,canonical > $SCRIPTPATH/../extras/dashboards/$DASHBOARD.json
